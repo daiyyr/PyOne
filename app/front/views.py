6ebcee905abeef5dcd327e250c476c83a7c8c1f1
Teemo <eru7921129@gmail.com>
2019-03-26 02:14:11 +1300

    multi password for root of each drive

--- app/front/views.py
+++ app/front/views.py
@@ -74,7 +74,7 @@ def index(path=None):
     if request.method=="POST":
         password1=request.form.get('password')
         #deal with root password
-        if len(path.split('/')) == 1 or n_path == '/':
+        if len(path.split(':')) == 1 or path.split(':')[1]=='/':
             for line in password.splitlines():
                 if line != '' and password1 == line:
                     password = line
