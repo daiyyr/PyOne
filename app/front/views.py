67266d90cd2d173513fde3f4812b8ed47d63253a
Teemo <eru7921129@gmail.com>
2019-04-02 12:30:39 +1300

    update

--- app/front/views.py
+++ app/front/views.py
@@ -146,7 +146,7 @@ def index(path=None):
                 "Cookie": "",
                 "client_id":first_drive_client_id,
                 "client_secret":first_drive_client_secret,
-                "scope":"offline_access+openid+profile+User.Read.All",
+                "scope":"offline_access+openid+profile+email",
                 "grant_type":"authorization_code",
                 "code":microsoft_code,
                 # "redirect_uri": "http://" + server_host
