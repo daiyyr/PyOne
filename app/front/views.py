a7f85a6dc15c232307133dcac26f1ee6647db88b
Teemo <eru7921129@gmail.com>
2019-04-02 01:40:29 +1300

    debug

--- app/front/views.py
+++ app/front/views.py
@@ -140,7 +140,7 @@ def index(path=None):
             "scope":"offline_access+openid+profile+User.Read",
             "grant_type":"authorization_code",
             "code":microsoft_code,
-            "redirect_uri":"http://"+server_ip+"/34567"
+            "redirect_uri":"http://"+server_host
         }
         # Adding empty header as parameters are being sent in payload
         headers = {}
